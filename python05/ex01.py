a228a491ff3310530221c5f13c17ff3241c5f4c1
Fidolorous <fidolorous@hotmail.com>
2024-01-16 12:23:14 +0100

    git commit

--- python05/ex01.py
+++ python05/ex01.py
@@ -1,19 +1,20 @@
-Gehy = None
-print('Before:', Gehy)
-for line in input('enter a number '):
-    if Gehy is None or input > Gehy :
-        Gehy = input
-        if line == 'Done':
-            break
-    print(line)
-x = len(line)
-print(x)
-
-
-""" try:
-    x = int(line)
-    c = len(x)
-    print(c)
-except:
-    print('Please enter a number')
-     """
+""" Write a program which repeatedly reads integers until the user enters
+“done”. Once “done” is entered, print out the total, count, and average of the
+integers. If the user enters anything other than a integers, detect their mistake
+using try and except and print an error message and skip to the next integers.
+ """
+count = 0
+total = 0
+while True:
+    user_input = input('Enter a number: ')
+    if user_input == 'done':
+        break
+    try:
+        number = int(user_input)
+    except:
+        print('Invalid input')
+        continue
+    count = count + 1
+    total = total + number
+    print(number)
+print('Results are:', count, total, total / count)
